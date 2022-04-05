78fe23224a3efb4870eb683d3aac152d194f29b9
Austin Sorrell <93938711+AustinDSorrell@users.noreply.github.com>
2022-04-05 09:12:58 -0700

    css prototype, more javascript

--- script.js
+++ script.js
@@ -2,15 +2,27 @@ window.onload = function(){
 
 let behindPlayer = {
     name: "player",
-    status: "affliction",
-    sanity: "mental",
-    inventory: "space",
+    status: "unafflicted",
+    sanity: "sane",
+    inventory: "empty",
     choices: []
+};
+
+let behindAlarm = {
+    time:"10:28am",
 };
 
 function nextArea(place) {
     moveArea = place;
 }
+
+//Non-story related variables
+
+var behindBranches = ["behindSL"];
+let behindNameStored = false;
+let behindPage = 
+
+//Construct function and other functions
 
 function areaConstruct(place) {
     let message = '<p name ="behindName"</p><p>'+ place.text+'</p>'
@@ -24,12 +36,26 @@ function areaConstruct(place) {
     }
     return message;
 }
-//Main Story
+
+function behindDataY(event) {
+
+    if (behindNameStored == false) {
+      let textName = gameData['playerName'].value;
+      let output = document.getElementById('output');
+      behindPlayer.name = textName;
+      behindNameStored = true;
+    }
+}
+
+bag.style.display = 'none'; //hides inventory
+
+//Main Story Variable
 
     var behindSL = {
 
         area1Open: {
-
+            text: "You are awoken to sound of a loud, blaring alarm beside your bed. You roll over and smack the top of the alarm silencing it. The time reads" + behindAlarm.time + "in bright red digital lettering before it dims slighlty. 'Time to get up I guess..",
+            options: [["getUp","Get Up"], ["setAlarm", "Set Alarm"], ["fallAsleep", "Fall Asleep"]],
         }
     }
 }
