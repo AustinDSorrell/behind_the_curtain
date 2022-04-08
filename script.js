882ddefadf1d217547de1412ba0697c29901caff
Austin Sorrell <93938711+AustinDSorrell@users.noreply.github.com>
2022-04-08 09:07:27 -0700

    Code from 6-8 of april

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function() {
 
+   bag.style.display = 'none';
+
 let behindPlayer = {
     name: "player",
     status: "unafflicted",
@@ -16,16 +18,16 @@ function nextArea(place) {
     moveArea = place;
 }
 
-//Non-story related variables
 
-var behindBranches = ["behindSL"];
-let behindNameStored = false;
-let behindPage = 
+//Story Page
+let behindPage = document.getElementById('enterName'),
+    behindData = behindPage.elements;
+
+console.log(behindData);
 
 //Construct function and other functions
-
-function areaConstruct(place) {
-    let message = '<p name ="behindName"</p><p>'+ place.text+'</p>'
+function behindNameConstruct(place) {
+    let message = '<p name ="playerName"</p><p>'+ place.text+'</p>'
     for (i = 0; i < place.options.length; i++) {
         message += '<button type = "submit" onclick="nextArea ('+ place.options[i][1]+ ')">' + place.options[i][0] + '</button>'
     }
@@ -37,25 +39,54 @@ function areaConstruct(place) {
     return message;
 }
 
+function statusUpdate() {
+    statsDiv = getElementsById("stats");
+    statsDiv.innerHTML = behindData;
+}
+
 function behindDataY(event) {
 
     if (behindNameStored == false) {
-      let textName = gameData['playerName'].value;
+      let behindTextStored = behindData['playerName'].value;
       let output = document.getElementById('output');
-      behindPlayer.name = textName;
+      behindPlayer.name = behindTextStored;
       behindNameStored = true;
     }
-}
-
-bag.style.display = 'none'; //hides inventory
 
 //Main Story Variable
 
     var behindSL = {
+
+        characterCreation: {
+            text: "Is the name " + behindPlayer.name + "correct?",
+            applicationCheck: [
+                []
+            ],
+            options: [
+                ["✓", "behindSL.area1Open"]
+            ]
+        },
 
         area1Open: {
             text: "You are awoken to sound of a loud, blaring alarm beside your bed. You roll over and smack the top of the alarm silencing it. The time reads" + behindAlarm.time + "in bright red digital lettering before it dims slighlty. 'Time to get up I guess..",
             options: [["getUp","Get Up"], ["setAlarm", "Set Alarm"], ["fallAsleep", "Fall Asleep"]],
         }
     }
+
+//Non-story related variables
+
+let continueArea = behindSL.characterCreation;
+var behindBranches = ["behindSL"];
+let behindNameStored = false;
+
+output.innerHTML = '${behindNameConstruct(nextArea)}';
+
+console.log(behindPlayer);
+
+event.preventDefault();
+
+}
+
+behindPage.addEventListener('submit', behindDataY);
+
 }
